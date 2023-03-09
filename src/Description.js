b58d611536945402a172e2a85fce359d9b830e17
emilamaj <git@github.com>
2023-03-09 21:21:04 +0100

    Added the book description view

---
+++ src/Description.js
@@ -0,0 +1,26 @@
+// This component is the first step of the app. It allows the user to enter the title and the description of the book
+import React from "react";
+import "./Description.css";
+
+const DescriptionView = ({ bookInfo, handleChange, nextStep }) => {
+  return (
+    <div className="container-form">
+        <p className="form-title">Book Title and Description</p>
+        <div className="form-element">
+            <label className="label-input" >Title</label>
+            <input className="input-single" type="text" name="title" value={bookInfo.title} onChange={handleChange} />
+        </div>
+
+        <div className="form-element">
+            <label className="label-input">Description</label>
+            <textarea className="input-multiple" type="text" name="description" value={bookInfo.description} onChange={handleChange} />
+        </div>
+
+        <div className="form-element-bottom">
+            <button className="button-form" type="button" onClick={nextStep}>Outline</button>
+        </div>
+    </div>
+  );
+};
+
+export default DescriptionView;
